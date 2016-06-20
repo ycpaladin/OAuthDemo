f28c65f50a47c6fea9f880d0904f15d5857ad59f
kevin.chen <ycpaladin@126.com>
2016-06-20 16:11:20 +0800

    submit

---
+++ ResourceServer/App_Start/HandlerConfig.cs
@@ -0,0 +1,18 @@
+﻿using ResourceServer.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Net.Http;
+using System.Web;
+
+namespace ResourceServer
+{
+    public class HandlerConfig
+    {
+        public static void RegisterHandlers(Collection<DelegatingHandler> handlers)
+        {
+            handlers.Add(new CorsMessageHandler());
+        }
+    }
+}
---
+++ ResourceServer/Controllers/MeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiContrib.Filters;
+
+namespace ResourceServer.Controllers
+{
+    [Authorize]
+    public class MeController : ApiController
+    {
+        [EnableCors]
+        public string Get()
+        {
+            return this.User.Identity.Name;
+        }
+    }
+}
